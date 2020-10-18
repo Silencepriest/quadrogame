04ce30c743933bf705d7bb9b82c422f478a4e1d4
Silencepriest <destroyer.rus@gmail.com>
2020-10-19 02:10:43 +0300

    some front page design

--- client/components/lobby.js
+++ client/components/lobby.js
@@ -5,24 +5,28 @@ const Lobby = () => {
   const [x, setX] = useState(5)
   const [y, setY] = useState(5)
   return (
-    <div>
-      <label htmlFor="xInput">Set X squares:</label>
+    <div className="flex flex-col justify-center items-center bg-green-400 w-screen h-screen">
+      <label className="text-2xl" htmlFor="xInput">
+        Set X squares:
+      </label>
       <input
         id="xInput"
-        className="mx-4 w-10 border-solid border-green-800 border-2"
+        className="mx-4 w-10 bg-green-300 p-2 text-xl"
         type="text"
         value={x}
         onChange={(e) => setX(e.target.value)}
       />
-      <label htmlFor="yInput">Set X squares:</label>
+      <label className="text-2xl" htmlFor="yInput">
+        Set Y squares:
+      </label>
       <input
         id="yInput"
-        className="mx-4 w-10 border-solid border-green-800 border-2"
+        className="mx-4 w-10 bg-green-300 p-2 text-xl"
         type="text"
         value={y}
         onChange={(e) => setY(e.target.value)}
       />
-      <Link to={`/game/${x}/${y}/${false}`}>Play</Link>
+      <Link className="px-8 py-2 mt-6 bg-red-600" to={`/game/${x}/${y}/${false}`}>Play</Link>
     </div>
   )
 }
